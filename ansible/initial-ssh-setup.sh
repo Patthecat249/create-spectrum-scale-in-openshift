36c119ab147de222554b7ac3f7a55f3d35bb9061
Patthecat249 <privat@patrickjahn.de>
2020-06-06 17:27:50 +0200

    Create automation script for Control-Node

---
+++ ansible/initial-ssh-setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# Setup for SPS-Node#1
+ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps1
+ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps1.home.local
+ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.241
+
+# Setup for SPS-Node#2
+ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps2
+ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps2.home.local
+ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.242
+
+# Setup for SPS-Node#3
+ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps3
+ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps3.home.local
+ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.243
