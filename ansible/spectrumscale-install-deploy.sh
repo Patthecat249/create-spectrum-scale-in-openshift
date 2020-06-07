e735b92ad3a9b0be202e7d32661a019d0abb8c69
Patthecat249 <privat@patrickjahn.de>
2020-06-07 03:15:31 +0200

    Scripts optimized

---
+++ ansible/spectrumscale-install-deploy.sh
@@ -0,0 +1,4 @@
+/usr/lpp/mmfs/5.0.5.0/installer/spectrumscale install --precheck
+/usr/lpp/mmfs/5.0.5.0/installer/spectrumscale install
+/usr/lpp/mmfs/5.0.5.0/installer/spectrumscale deploy --precheck
+/usr/lpp/mmfs/5.0.5.0/installer/spectrumscale deploy
