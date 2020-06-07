c2655a6e9096af23db834f65b6c9feb847a23f43
Patthecat249 <privat@patrickjahn.de>
2020-06-07 12:14:35 +0200

    add rootpassword-file

--- ansible/initial-ssh-setup.sh
+++ ansible/initial-ssh-setup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 # Setup for SPS-Node#1
-sshpass -p Test1234 ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps1
-sshpass -p Test1234 ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps1.home.local
-sshpass -p Test1234 ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.241
+sshpass -f rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps1
+sshpass -f rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps1.home.local
+sshpass -f rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.241
 
 # Setup for SPS-Node#2
-sshpass -p Test1234 ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps2
-sshpass -p Test1234 ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps2.home.local
-sshpass -p Test1234 ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.242
+sshpass -f rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps2
+sshpass -f rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps2.home.local
+sshpass -f rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.242
 
 # Setup for SPS-Node#3
-sshpass -p Test1234 ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps3
-sshpass -p Test1234 ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps3.home.local
-sshpass -p Test1234 ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.243
+sshpass -f rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps3
+sshpass -f rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps3.home.local
+sshpass -f rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.243
