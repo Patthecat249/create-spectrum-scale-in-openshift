4c32f797e9ab5dacd0b34faa69fbd282f149eb48
Patthecat249 <privat@patrickjahn.de>
2021-04-26 21:46:08 +0200

    create shell script

--- ansible/spectrumscale-install-deploy.sh
+++
@@ -1,4 +0,0 @@
-/usr/lpp/mmfs/5.1.0.3/installer/spectrumscale install --precheck
-/usr/lpp/mmfs/5.1.0.3/installer/spectrumscale install
-/usr/lpp/mmfs/5.1.0.3/installer/spectrumscale deploy --precheck
-/usr/lpp/mmfs/5.1.0.3/installer/spectrumscale deploy
