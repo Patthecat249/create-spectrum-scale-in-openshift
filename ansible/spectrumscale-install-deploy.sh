bb160a33baf17614c43f2e741a9bd43c6f105fd2
Patthecat249 <privat@patrickjahn.de>
2021-04-26 21:28:36 +0200

    let the game begin

--- ansible/spectrumscale-install-deploy.sh
+++ ansible/spectrumscale-install-deploy.sh
@@ -1,4 +1,4 @@
-/usr/lpp/mmfs/5.0.5.0/installer/spectrumscale install --precheck
-/usr/lpp/mmfs/5.0.5.0/installer/spectrumscale install
-/usr/lpp/mmfs/5.0.5.0/installer/spectrumscale deploy --precheck
-/usr/lpp/mmfs/5.0.5.0/installer/spectrumscale deploy
+/usr/lpp/mmfs/5.1.0.3/installer/spectrumscale install --precheck
+/usr/lpp/mmfs/5.1.0.3/installer/spectrumscale install
+/usr/lpp/mmfs/5.1.0.3/installer/spectrumscale deploy --precheck
+/usr/lpp/mmfs/5.1.0.3/installer/spectrumscale deploy
