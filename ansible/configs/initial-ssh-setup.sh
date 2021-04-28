d91008c70c9fe82862095d6c9865cf02a12a674e
Patthecat249 <privat@patrickjahn.de>
2021-04-28 12:41:35 +0200

    configs/rootpassword

--- ansible/configs/initial-ssh-setup.sh
+++ ansible/configs/initial-ssh-setup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 echo "" > /root/.ssh/known_hosts
 # Setup for SPS-Node#1
-sshpass -f ../configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick1
-sshpass -f ../configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick1.home.local
-sshpass -f ../configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.161
+sshpass -f configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick1
+sshpass -f configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick1.home.local
+sshpass -f configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.161
 
 # Setup for SPS-Node#2
-sshpass -f ../configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick2
-sshpass -f ../configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick2.home.local
-sshpass -f ../configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.162
+sshpass -f configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick2
+sshpass -f configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick2.home.local
+sshpass -f configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.162
 
 # Setup for SPS-Node#3
-sshpass -f ../configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick3
-sshpass -f ../configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick3.home.local
-sshpass -f ../configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.163
+sshpass -f configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick3
+sshpass -f configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@sps-patrick3.home.local
+sshpass -f configs/rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@10.0.249.163
